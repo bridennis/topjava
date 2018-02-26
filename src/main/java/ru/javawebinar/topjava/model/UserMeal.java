d0983ab1e6d4cf3f49da63f3c05019ba1a88c753
bridennis <5626164+bridennis@users.noreply.github.com>
2018-02-26 23:46:03 +0300

    HW0 implemented on stream

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -1,33 +1,39 @@
 package ru.javawebinar.topjava.model;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 /**
- * GKislin
- * 11.01.2015.
+ * GKislin 11.01.2015.
  */
 public class UserMeal {
-    private final LocalDateTime dateTime;
 
-    private final String description;
+  private final LocalDateTime dateTime;
 
-    private final int calories;
+  private final String description;
 
-    public UserMeal(LocalDateTime dateTime, String description, int calories) {
-        this.dateTime = dateTime;
-        this.description = description;
-        this.calories = calories;
-    }
+  private final int calories;
 
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
+  public UserMeal(LocalDateTime dateTime, String description, int calories) {
+    this.dateTime = dateTime;
+    this.description = description;
+    this.calories = calories;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public LocalDateTime getDateTime() {
+    return dateTime;
+  }
 
-    public int getCalories() {
-        return calories;
-    }
+  public LocalDate getDate() {
+    return dateTime.toLocalDate();
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public int getCalories() {
+    return calories;
+  }
+
 }
