f5dfa30c4c65adcc0cf8ff3a8237f2950dab5376
bridennis <5626164+bridennis@users.noreply.github.com>
2018-03-05 20:12:38 +0300

    HW1.2 implemented

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -3,11 +3,15 @@ package ru.javawebinar.topjava.web;
 import static org.slf4j.LoggerFactory.getLogger;
 
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
@@ -16,6 +20,14 @@ public class MealServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
 
+        List<MealWithExceed> mealWithExceedList = MealsUtil.getFilteredWithExceeded(
+            MealsUtil.meals,
+            LocalTime.MIN,
+            LocalTime.MAX,
+            MealsUtil.caloriesPerDay
+        );
+
+        request.setAttribute("mealWithExceedList", mealWithExceedList);
         request.getRequestDispatcher("/meals.jsp").forward(request, response);
 //        response.sendRedirect("meals.jsp");
     }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,10 +1,34 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib uri="http://sargue.net/jsptags/time" prefix="javatime" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>Meals</title>
+    <style>
+        .red_row { background: red; color: white; }
+        .green_row { background: green; color: white; }
+    </style>
 </head>
 <body>
 <h3><a href="index.html">Home</a></h3>
 <h2>Meals</h2>
+<table>
+    <tr>
+        <th>Date/time</th>
+        <th>Description</th>
+        <th>Calories</th>
+        <th>Limit</th>
+    </tr>
+    <jsp:useBean id="mealWithExceedList" scope="request" type="java.util.ArrayList"/>
+    <c:forEach var="meal" items="${mealWithExceedList}">
+    <tr class="<c:out value="${meal.isExceed() ? 'red_row': 'green_row'}"/>">
+        <td><javatime:format value="${meal.getDateTime()}" pattern="yyyy-MM-dd HH:mm" /></td>
+        <td>${meal.getDescription()}</td>
+        <td>${meal.getCalories()}</td>
+        <td>${meal.isExceed()}</td>
+    </tr>
+    </c:forEach>
+
+</table>
 </body>
 </html>
